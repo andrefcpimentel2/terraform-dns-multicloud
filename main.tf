8517f7a7daf75e224c7462347fdffeb0c80d78ad
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2020-07-29 11:38:49 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,31 @@
 terraform {
   required_version = ">= 0.12.4"
-  required_providers {
-    aws     = ">= 2.46"
-    google  = ">= 3.5"
-    azurerm = ">= 1.42"
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "dns-multicloud-org"
+    workspaces {
+      name = "dns-multicloud"
+    }
   }
+}
+
+# AWS General Configuration
+
+provider "aws" {
+  version = "~> 2.0"
+  region  = var.aws_region
+}
+
+# GCP General Configuration
+
+provider "google" {
+  version = "~> 2.9"
+  project = var.gcp_project
+  region  = var.gcp_region
+}
+
+# Azure General Configuration
+
+provider "azurerm" {
+  version = "~>1.32.1"
 }
