122ec86a35545c063c505b11a6f4885cab1b03d1
Andre Pimentel <49865323+andrefcpimentel2@users.noreply.github.com>
2020-11-17 11:02:54 +0000

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,27 +1,27 @@
 output "aws_sub_zone_id" {
-  value = aws_route53_zone.aws_sub_zone.*.zone_id
+  value = aws_route53_zone.aws_sub_zone[0].zone_id
 }
 
 output "aws_sub_zone_nameservers" {
-  value = aws_route53_zone.aws_sub_zone.*.name_servers
+  value = aws_route53_zone.aws_sub_zone[0].name_servers
 }
 
 output "azure_sub_zone_name" {
-  value = azurerm_dns_zone.azure_sub_zone.*.id
+  value = azurerm_dns_zone.azure_sub_zone[0].id
 }
 
 output "azure_sub_zone_nameservers" {
-  value = azurerm_dns_zone.azure_sub_zone.*.name_servers
+  value = azurerm_dns_zone.azure_sub_zone[0].name_servers
 }
 
 output "azure_dns_resourcegroup" {
-  value = azurerm_resource_group.dns_resource_group.*.name
+  value = azurerm_resource_group.dns_resource_group[0].name
 }
 
 output "gcp_dns_zone_name" {
-  value = google_dns_managed_zone.gcp_sub_zone.*.name
+  value = google_dns_managed_zone.gcp_sub_zone[0].name
 }
 
 output "gcp_dns_zone_nameservers" {
-   value = google_dns_managed_zone.gcp_sub_zone.*.name_servers
+   value = google_dns_managed_zone.gcp_sub_zone[0].name_servers
 }
